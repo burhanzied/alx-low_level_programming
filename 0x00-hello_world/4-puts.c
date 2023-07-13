5c451c4ff0e14878bbec93911043e9b965024d53
burhanzied <ziedburhan@gmail.com>
2023-07-13 12:36:06 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 * Main - Entry point
 *
-* Return Always (success)
+* Return Always 0 (success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
