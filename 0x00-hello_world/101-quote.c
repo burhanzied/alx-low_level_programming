edbf5d58538c71e32b8486656592edd72689560d
burhanzied <ziedburhan@gmail.com>
2023-07-13 16:53:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- *main - Entry point
- *Return: 1 if success
+ * main - entry point
+ * Return: success
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	char ch[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, ch, sizeof(ch));
 	return (1);
 }
